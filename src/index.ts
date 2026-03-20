e353ffd5844aad8725c8ab6293fda3c75b546ee3
Ethan Liu <eliu3152@gmail.com>
2026-03-19 23:00:00 -0400

    Work on sidebar collapse

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
 import "./styles.css"
+
+const sidebar = document.querySelector(".sidebar");
+const sidebar_collapse_button = document.querySelector(".sidebar-collapse-button");
+sidebar_collapse_button?.addEventListener("click", e => {
+    sidebar?.classList.toggle("collapsed");
+});
