3e58889d25168050f983cae4b4cea5088a151a3c
Ethan Liu <eliu3152@gmail.com>
2026-03-22 23:28:14 -0400

    Tweak animation for projects collapsable

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,9 @@ sidebar_collapse_button?.addEventListener("click", e => {
 });
 
 const projects_header = document.querySelector(".projects-header");
-const projects_section = document.querySelector(".projects-section")
+const projects_section = document.querySelector(".projects-section");
+const projects_container = document.querySelector<HTMLDivElement>(".projects-container");
 projects_header?.addEventListener("click", e => {
     projects_section?.classList.toggle("collapsed");
+    
 });
